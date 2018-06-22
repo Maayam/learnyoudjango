b2b81fb78b4c7e4df5afea103be5fef0f8d30f8f
Antoine DEVOS <antoine.devos.francis@gmail.com>
2018-06-22 12:53:15 +0200

    first commit

---
+++ learnyoudjango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\antoine\python\learnyoudjango\learnyoudjango\scripts\python3.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
